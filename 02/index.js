124997824feffbc4c80914fe5dc5617adbed911b
XPL <616954589@qq.com>
2019-10-13 21:05:13 +0800

    init

---
+++ 02/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by XPL on 2019/10/13.
+ */
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Vue 表单管理Demo!',
+        habit:[],
+        age: 20,
+        name: 'Tony'
+    }
+})
