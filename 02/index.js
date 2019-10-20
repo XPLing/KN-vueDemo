bc82336b6fd46c6cb24975277b5fcb5367ecd6b8
XPL <616954589@qq.com>
2019-10-20 23:04:29 +0800

    add jquery form

--- 02/index.js
+++ 02/index.js
@@ -2,12 +2,26 @@
  * Created by XPL on 2019/10/13.
  */
 
-var app = new Vue({
-    el: '#app',
+;(function () {
+  var app = new Vue({
+    el: '#vueApp',
     data: {
-        message: 'Vue 表单管理Demo!',
-        habit:[],
-        age: 20,
-        name: 'Tony'
+      message: 'Vue Form!',
+      habit: [],
+      age: 20,
+      name: 'Tony'
     }
-})
+  });
+  $('#jqueryApp').on('input', 'input', function (e) {
+    var target = $(this),
+      name = target.attr('name'),
+      val = target.val();
+    if (name === 'habit') {
+      val = $('[type="checkbox"][name="habit"]:checked');
+      val = Array.from(val);
+      val = val.map(val => $(val).val()).join(',');
+    }
+    $('#' + name + 'Val').html(val);
+  });
+}());
+
